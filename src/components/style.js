81ec151298f5c8ec9a6680f8e42add237b076efd
Ammar <binfaisal.ammar@gmail.com>
2018-11-18 20:21:28 +0530

    updates

---
+++ src/components/style.js
@@ -0,0 +1,20 @@
+const columnify = {
+	fontFamily: "Ubuntu, 'Open Sans', 'Comic Sans', sans-serif",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	height: "100vh",
+	padding: "8vh 5vw",
+	marginTop: "-20vh"
+};
+
+const heading = {
+	fontSize: "calc(1.4em + .5vw + .8vh)", margin: "3vh 0"
+}
+
+const para = {
+	fontSize: "calc(.8em + .3vw + .6vh)"
+}
+
+export default {columnify, heading, para};
--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Layout from "../components/Layout";
 import Helmet from "react-helmet";
-
+import Navigation from "../components/Navigation";
+import Layout from "../components/Layout"
 // import "../../sass/about.sass"
-import columnify from "../styles/style";
+import styles from "../components/style";
 
 const About = () => (
-	<Layout id="about-us" style={{ fontFamily: "Ubuntu, 'open sans', roboto, sans-serif", ...columnify }}>
+	<Layout id="about-us" style={styles.columnify}>
 		<Helmet>
-			<title>About us</title>
+			<title>About Us | CMS ALIGANJ</title>
 		</Helmet>
-		<h1 style={{ fontSize: "2.4em", margin: "3vh 0" }}>Havent made this page yet :D</h1>
-		<p style={{ fontSize: "1.4em" }}>Comeback later</p>
+		<Navigation />
+		<h1 style={styles.heading}>Havent made this page yet :D</h1>
+		<p style={styles.para}>Comeback later</p>
 	</Layout>
 );
 
