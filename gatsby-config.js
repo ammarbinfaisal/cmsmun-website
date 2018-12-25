c3faa408580881c332ebcb7f868bb69a7d5f806a
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2018-12-25 14:04:43 +0530

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,12 @@ module.exports = {
             resolve: `gatsby-plugin-manifest`,
             options: {
                 name: "CMS Aliganj's Model United Nations",
-                short_name: "cms mun",
+                short_name: "cmsmun aliganj",
                 start_url: "/",
-                background_color: "#241f1c",
-                theme_color: "#241f1c",
+                background_color: "#1e1e1e",
+                theme_color: "#1e1e1e",
                 display: "minimal-ui",
-                icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+                icon: "src/images/logo.png", // This path is relative to the root of the site.
             },
         },
         {
