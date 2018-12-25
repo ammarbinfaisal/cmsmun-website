f2c8e949b1655cfe2d4f9634c1c388fc02ba313f
Ammar <binfaisal.ammar@gmail.com>
2018-12-25 14:07:58 +0530

    add about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,16 +1,32 @@
 import React from "react";
 import Helmet from "react-helmet";
+import Nav from "../components/Navigation";
+import Layout from "../components/Layout";
 
-import "../sass/page.sass";
+import "../sass/aboutus.sass";
 
 const About = () => (
-	<div id="about-us" className="plain-page">
+	<Layout id="about-us">
 		<Helmet>
 			<title>About Us | CMS ALIGANJ</title>
 		</Helmet>
-		<h1>Haven't made this page yet :D</h1>
-		<p>Comeback later</p>
-	</div>
+		<div className="white-overlay black">
+			<div className="questrial s9em fw700">
+				<h1>About us</h1>
+				<p className="text">
+					City Montessori School (CMS) is the world’s largest school, with over 55,000 students and 4,500 staff across
+					18 campuses in the city of Lucknow. Founded in 1959 with 5 children on a borrowed capital of 300 Rupees by
+					Dr Jagdish Gandhi and Dr Bharti Gandhi, CMS today is an ICSE- and ISC-affiliated school whose students from
+					Montessori to Class XII achieve some of the highest exam results in India. Beyond these achievements, Drs
+					Jagdish and Bharti Gandhi started CMS with vision to inspire generations of children to become socially
+					motivated and conscious citizens of India and the world. Their motto “Jai Jagat” - 'Hail the World', in
+					contrast to the popular post-Independence greeting 'Jai Hind' ('Hail India') at the time of CMS's foundation
+					- has been the school's greeting from the beginning. CMS therefore places great emphasis on children's
+					all-round development, moral character, and international exposure.
+		</p>
+			</div>
+		</div>
+	</Layout>
 );
 
 export default About;
