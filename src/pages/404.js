50d57597bea2f37918bda1a76bb084eab515e09d
Ammar <binfaisal.ammar@gmail.com>
2019-02-12 01:54:34 +0530

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,14 @@ import Helmet from "react-helmet";
 import "../sass/page.sass";
 
 const NotFoundPage = () => (
-	<div id="404" className="plain-page">
+	<div id="404" className="404-page" style={{backgroundColor: "rgba(255, 255, 255, 0.8)"}}>
 		<Helmet>
 			<title>Page not found</title>
 		</Helmet>
-		<h1>NOT FOUND</h1>
+		<h1>404</h1><h2>PAGE NOT FOUND</h2>
 		<p>
 			You just hit a route that doesn&#39;t exist...
-			<br /> & the sadness.
+			<br /> &amp; the sadness.
 		</p>
 	</div>
 );
