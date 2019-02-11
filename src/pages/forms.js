fa2641362098f81c9e2c142c5cf641634be566b7
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2019-02-11 22:47:51 +0530

    add forms page

---
+++ src/pages/forms.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../components/Layout";
+
+const About = () => (
+	<Layout id="about-us">
+		<Helmet>
+			<title>About Us | CMS ALIGANJ</title>
+		</Helmet>
+		<div className="white-overlay black">
+			<div className="questrial s9em fw700">
+				<h1>Forms</h1>
+				<div className="centerize" style={{padding: "50px", lineHeight: "2"}}>
+                <a
+						href="https://docs.google.com/forms/d/e/1FAIpQLSctF4ZZxFENsaHI7r_OoJyUCjS5xRhI-gURrUqXei-jklAD4Q/viewform"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Individual Delegation
+					</a>
+					<a
+						href="https://docs.google.com/forms/d/1jVAnq1yi8_8XIGBD2CS0UkP1xT_rNI6nFOrHGwwejgY/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						International Press
+					</a>
+                </div>
+			</div>
+		</div>
+	</Layout>
+);
+
+export default About;
