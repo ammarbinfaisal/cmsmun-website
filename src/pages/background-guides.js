23b706c81e00d06981ff0bab98d074180640336a
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-04-10 16:14:27 +0530

    add bgunga and bgunhrc

---
+++ src/pages/background-guides.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../components/Layout";
+
+const BackgroundGuide = () => (
+	<Layout id="background-guides">
+		<Helmet>
+			<title>Background Guide | CMSMUN ALIGANJ</title>
+			<meta name="description" content="Background Guide | CMSMUN ALIGANJ" />
+		</Helmet>
+		<div className="white-overlay black full-page questrial s9em">
+			<h1>Background Guides</h1>
+			<div style={{display: "Flex", flexWrap: "wrap", margin: "0 12px"}}>
+			<a href="/bg-unga.pdf" className="bgguide">UNGA</a>
+			<a href="/bg-unhrc.pdf" className="bgguide">UNHRC</a>
+			<a href="/.pdf" className="bgguide">UNSC</a>
+			<a href="/.pdf" className="bgguide">UNCSW</a>
+			<a href="/.pdf" className="bgguide">JCC</a>
+			<a href="/.pdf" className="bgguide">AIPPM</a>
+			</div>
+	</div>
+    </Layout>
+);
+
+export default BackgroundGuide;
